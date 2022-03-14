d34df54777a70c037019f30216b990a7abe8cbec
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-03-13 23:02:50 -0300

    mathModule

---
+++ mathModule/calcFunc.js
@@ -0,0 +1,9 @@
+var sumFunc = require("./sum");
+var subFunc = require("./sub");
+var divFunc = require("./div");
+var multiFunc = require("./multi");
+
+console.log(sumFunc(5,5));
+console.log(subFunc(10,5));
+console.log(divFunc(100,10));
+console.log(multiFunc(10,150));
