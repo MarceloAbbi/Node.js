d79d7a459abdc108bb0c7bc00640068a6175090a
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-03-13 22:35:57 -0300

    calc

---
+++ calc.js
@@ -0,0 +1,17 @@
+function sum(a,b){
+  return a + b;
+}
+
+function sub(a,b){
+  return a - b;
+}
+
+function multi(){
+  return a * b;
+}
+
+function divis(){
+  return a / b;
+}
+
+console.log(sub(10,5));
